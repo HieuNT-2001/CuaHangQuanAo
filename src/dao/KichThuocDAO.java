be74d530952af1f4a12655ab2c995f25c92e8ede
HieuNT-2001 <hieuth0109@gmail.com>
2024-07-25 22:34:41 +0700

    Sua lai class DAO

--- src/dao/KichThuocDAO.java
+++ src/dao/KichThuocDAO.java
@@ -3,62 +3,68 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+
 import entity.KichThuoc;
 import utils.Xjdbc;
 import java.util.List;
 import java.util.ArrayList;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 /**
  *
  * @author PC
  */
 public class KichThuocDAO {
+
     private final String SELECT_ALL = "SELECT * FROM KichThuoc";
-    private final String SELECT_BY_ID = "SELECT * FROM KichThuoc WHERE MaKT";
+    private final String SELECT_BY_ID = "SELECT * FROM KichThuoc WHERE MaKT = ?";
     private final String INSERT = "INSERT INTO KichThuoc (KichThuoc) values ?";
     private final String UPDATE = "UPDATE KichThuoc SET KichThuoc = ? WHERE MaKT = ?";
     private final String DELETE = "DELETE FROM KichThuoc WHERE MaKT = ?";
-    
+
     //Insert vào bảng kích thước
-    public void insert(KichThuoc entity){
-        Xjdbc.update(INSERT, 
-                entity.getKichThuoc());
+    public void insert(KichThuoc entity) {
+        Xjdbc.update(INSERT,
+                entity.getKichThuoc()
+        );
     }
-  
+
     //Update vào bảng kích thước
-    public void update(KichThuoc entity){
-        Xjdbc.update(UPDATE, 
-                entity.getKichThuoc());
+    public void update(KichThuoc entity) {
+        Xjdbc.update(UPDATE,
+                entity.getKichThuoc(),
+                entity.getMaKT()
+        );
     }
-    
+
     //Delete kích thước
     public void delete(int id) {
         Xjdbc.update(DELETE, id);
     }
-    
+
     //Select màu sắc theo câu lệnh sql
-    public List<KichThuoc> SelectBySql(String sql, Object...args){
+    public List<KichThuoc> SelectBySql(String sql, Object... args) {
         List<KichThuoc> list = new ArrayList<>();
-        try (ResultSet rs = Xjdbc.query(sql, args)){
-            while(rs.next()) {
+        try (ResultSet rs = Xjdbc.query(sql, args)) {
+            while (rs.next()) {
                 KichThuoc entity = new KichThuoc();
                 entity.setMaKT(rs.getInt("MaKT"));
                 entity.setKichThuoc(rs.getString("KichThuoc"));
                 list.add(entity);
             }
         } catch (SQLException e) {
-            System.out.println(e); 
+            System.out.println(e);
             throw new RuntimeException(e);
         }
         return list;
     }
-    
+
     //Select toàn bộ màu sắc
-    public List<KichThuoc> SelectAll(){
+    public List<KichThuoc> SelectAll() {
         return SelectBySql(SELECT_ALL);
     }
-    
+
     //Select kích thước theo mã
     public KichThuoc SelectById(int id) {
         return SelectBySql(SELECT_BY_ID, id).get(0);
--- src/dao/NhanVienDAO.java
+++ src/dao/NhanVienDAO.java
@@ -3,62 +3,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+
 import entity.NhanVien;
 import utils.*;
 import java.util.*;
 import java.sql.*;
+
 /**
  *
  * @author HP
  */
 public class NhanVienDAO {
-    public void insert(NhanVien nv){
-        String sql="INSERT INTO NhanVien (TenDangNhap, MatKhau, HoTen, NamSinh, SDT, CCCD, VaiTro)\n" +
-        "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        Xjdbc.update(sql, nv.getTenDangNhap(),nv.getMatKhau(),nv.getHoTen(),
-                nv.getNamSinh(),nv.getSdt(),nv.getCccd(),nv.isVaiTro());
+
+    public void insert(NhanVien nv) {
+        String sql = "INSERT INTO NhanVien (TenDangNhap, MatKhau, HoTen, NamSinh, SDT, CCCD, VaiTro)\n"
+                + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        Xjdbc.update(sql, nv.getTenDangNhap(), nv.getMatKhau(), nv.getHoTen(),
+                nv.getNamSinh(), nv.getSdt(), nv.getCccd(), nv.isVaiTro());
     }
-    public void update(NhanVien nv){
-        String sql="UPDATE NhanVien SET TenDangNhap=?, MatKhau=?, HoTen=?, NamSinh=?, SDT=?, CCCD=?, VaiTro=?\n" +
-        "WHERE MaNV=?";
-        Xjdbc.update(sql, nv.getTenDangNhap(),nv.getMatKhau(),nv.getHoTen(),
-                nv.getNamSinh(),nv.getSdt(),nv.getCccd(),nv.isVaiTro(),nv.getMaNV());
+
+    public void update(NhanVien nv) {
+        String sql = "UPDATE NhanVien SET TenDangNhap=?, MatKhau=?, HoTen=?, NamSinh=?, SDT=?, CCCD=?, VaiTro=?\n"
+                + "WHERE MaNV=?";
+        Xjdbc.update(sql, nv.getTenDangNhap(), nv.getMatKhau(), nv.getHoTen(),
+                nv.getNamSinh(), nv.getSdt(), nv.getCccd(), nv.isVaiTro(), nv.getMaNV());
     }
-    public void delete(String maNV){
-        String sql="DELETE FROM NhanVien WHERE MaNV=?";
+
+    public void delete(int maNV) {
+        String sql = "DELETE FROM NhanVien WHERE MaNV=?";
         Xjdbc.update(sql, maNV);
     }
-    public List<NhanVien> select(){
-        String sql="SELECT * FROM NhanVien";
-        return select(sql);
+
+    public List<NhanVien> select() {
+        String sql = "SELECT * FROM NhanVien";
+        return selectBySql(sql);
     }
-    public NhanVien findByID(String maNV){
-        String sql="SELECT * FROM NhanVien WHERE MaNV=?";
-        List<NhanVien> lnv=select(sql, maNV);
-        return lnv.size()>0 ? lnv.get(0) : null;
+
+    public NhanVien selectById(int maNV) {
+        String sql = "SELECT * FROM NhanVien WHERE MaNV=?";
+        List<NhanVien> lnv = selectBySql(sql, maNV);
+        return lnv.size() > 0 ? lnv.get(0) : null;
     }
-    private List<NhanVien> select(String sql, Object...args){
-        List<NhanVien> lnv=new ArrayList<>();
-        try{
+
+    private List<NhanVien> selectBySql(String sql, Object... args) {
+        List<NhanVien> lnv = new ArrayList<>();
+        try {
             ResultSet rs = null;
-            try{
-                rs=Xjdbc.query(sql, args);
-                while(rs.next()){
-                    NhanVien nv=readFromResultSet(rs);
+            try {
+                rs = Xjdbc.query(sql, args);
+                while (rs.next()) {
+                    NhanVien nv = readFromResultSet(rs);
                     lnv.add(nv);
                 }
-            }
-            finally{
+            } finally {
                 rs.getStatement();
             }
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return lnv;
     }
-    private NhanVien readFromResultSet(ResultSet rs) throws SQLException{
-        NhanVien nv=new NhanVien();
+
+    private NhanVien readFromResultSet(ResultSet rs) throws SQLException {
+        NhanVien nv = new NhanVien();
         nv.setMaNV(rs.getInt("MaNV"));
         nv.setTenDangNhap(rs.getString("TenDangNhap"));
         nv.setMatKhau(rs.getString("MatKhau"));
